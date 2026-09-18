5fdc8f2e98010be74e582db6939b7d40beddf45c
AndresB <andres_bs@hotmail.com>
2026-09-18 12:54:28 +0200

    Add Spring Boot backend scaffold (awg-tracking).

---
+++ awg-tracking/src/main/java/com/artwithgab/awg_tracking/AwgTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.artwithgab.awg_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwgTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwgTrackingApplication.class, args);
+	}
+
+}
